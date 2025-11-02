2ac6d7f795083b824dce17c6e4205f71128e014f
Eva Belanger <eva.belanger@epitech.eu>
2025-11-02 13:47:31 +0100

    <add> Add header file with all functions that I have for the moment

---
+++ include/my.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** my
+** File description:
+** Header file
+*/
+
+#ifndef MY_
+    #define MY_
+
+void my_putchar(char c);
+void my_puterror(char *str);
+void my_putstr(char *str);
+
+int count_flags(char *av);
+
+#endif /* MY_ */
