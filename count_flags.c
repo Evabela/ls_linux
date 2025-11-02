4cc2591bc93a281c7fb208e4d5c6686d876130be
Eva Belanger <eva.belanger@epitech.eu>
2025-11-02 13:38:36 +0100

    <add> initialize count_flags

---
+++ count_flags.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** count_flags
+** File description:
+** Search and repertory flags, if error, return 84
+*/
+#include "include/my.h"
+
+int count_flags(char *av)
+{
+    return 0;
+}
