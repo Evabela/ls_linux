0a56e1c3e9cbf47008a37a627144565b810db5ce
Eva Belanger <eva.belanger@epitech.eu>
2025-11-03 23:26:10 +0100

    <add> New test to test function sort_array

---
+++ tests/test_sort_array.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** test_sort_array.c
+** File description:
+** Test for sort a double array of strings
+*/
+
+#include "../include/my.h"
+#include <criterion/criterion.h>
+
+Test (sort_array, whith_differents_words)
+{
+    char *arr[9] = {"Hello", "world", "abcd", "Abdc2", "42", "How", "How", "ZER", "aaaaa"};
+    char *attempt[9] = {"42", "Abdc2", "Hello", "How", "How", "ZER", "aaaaa", "abcd", "world"};
+
+    cr_assert_str_eq(sort_array(arr, 9), attempt);
+}
+
+Test (sort_array, with_no_words)
+{
+    char **arr;
+    char **attempt;
+
+    cr_assert_str_eq(sort_array(arr, 0), attempt);
+}
+
+Test(sort_array, with_same_words)
+{
+    char *arr[3] = {"Hello", "Hello", "Hello"};
+    char *attempt[3] = {"Hello", "Hello", "Hello"};
+
+    cr_assert_str_eq(sort_array(arr, 3), attempt);
+}
