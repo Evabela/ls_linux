15633217827d47f83ec5934f60da30e5e3d0f3b2
Eva Belanger <eva.belanger@epitech.eu>
2025-11-03 15:52:49 +0100

    <add> Putting all librairy files in a folder named lib (for the moment we have my_putstr, my_putchar, my_puterror and my_strlen)

---
+++ lib/my_len.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** my_len
+** File description:
+** All functions to have the len of something (int, long or str if one is necessary)
+*/
+
+#include "../include/my.h"
+
+int my_strlen(char *str)
+{
+    int i = 0;
+    int count;
+
+    while (str[i] != 0){
+        i++;
+        count++;
+    }
+    return count;
+}
