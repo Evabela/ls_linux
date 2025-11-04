ecc0bd38a2c2d183c353d443675f1f7a64b16c4e
Eva Belanger <eva.belanger@epitech.eu>
2025-11-04 22:23:20 +0100

    <bug> Function for sorting the list of files and folders finished and debug

--- display_ls.c
+++ display_ls.c
@@ -9,25 +9,53 @@
 #include <dirent.h>
 #include <stdlib.h>
 
-static void my_swap_str(char *str1, char *str2)
+static void print_2d_arr(char **str, int size)
 {
-    char *temp;
-
-    if (my_strcmp(str1, str2) == 1){
-        temp = my_strdup(temp, str1);
-        str1 = str2;
-        str2 = temp;
-    }
-    free(temp);
-}
-
-void **sort_array(char **list_files_ns, int nb_files)
-{
-    for (int i = 0; i < nb_files - 1; i++){
-        for (int j = 0; j < nb_files - 1; j++){
-            my_swap_str(list_files_ns[i], list_files_ns[i + 1]);
+    for (int i = 0; i < size; i++){
+        if (str[i] == 0){
+            my_put_nbr(0);
+            my_putstr(" / ");
+        } else {
+            my_putstr(str[i]);
+            my_putstr(" / ");
         }
     }
+    my_putchar('\n');
+}
+
+static int find_smallest(char **list, int nb_files)
+{
+    int smaller = 0;
+    int comp = 0;
+
+    for (int i = 0; i < nb_files; i++){
+        comp = 0;
+        while (list[smaller] == 0)
+            smaller++;
+        if (list[i] != 0 && list[smaller] != 0)
+            comp = my_strcmp(list[i], list[smaller]);
+        if (comp == -1)
+            smaller = i;
+    }
+    return smaller;
+}
+
+char **sort_array(char **list_files_ns, int nb_files)
+{
+    char **sorted;
+    int k = 0;
+
+    sorted = malloc(sizeof(char *) * nb_files);
+    for (int i = 0; i < nb_files; i++){
+        k = find_smallest(list_files_ns, nb_files);
+        if (list_files_ns[k] != 0){
+            sorted[i] = malloc(sizeof(char) *
+                (my_strlen(list_files_ns[k]) + 1));
+            my_strcpy(sorted[i], list_files_ns[k]);
+            list_files_ns[k] = 0;
+        }
+    }
+    return sorted;
 }
 
 char **get_files(char *pathname, int nb_files)
@@ -58,7 +86,7 @@ int count_files(char *pathname)
 
     list_files = opendir(pathname);
     read_file = readdir(list_files);
-    while(read_file != 0){
+    while (read_file != 0){
         nb_files++;
         read_file = readdir(list_files);
     }
