6190574d9f959ded70a84bf663dd1f630259e204
Eva Belanger <eva.belanger@epitech.eu>
2025-11-04 10:45:45 +0100

    <add> New function : my_strlowcase (transform a string char in lowercase letters)

---
+++ lib/my_strlowcase.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** my_strlowcase
+** File description:
+** replace all non-lowcase alpha by lowcases characters
+*/
+#include "../include/my.h"
+
+char *my_strlowcase(char *str)
+{
+    for (int i = 0; i < my_strlen(str); i++){
+        if (str[i] <= 'Z' && str[i] >= 'A')
+            str[i] = str[i] + 32;
+    }
+    return str;
+}
