5f32d7b8f903b44bd81c3eed659c3ae25c658470
Eva Belanger <eva.belanger@epitech.eu>
2025-11-04 22:30:28 +0100

    <add> my_put_nbr added

--- lib/my_putchar.c
+++ lib/my_putchar.c
@@ -31,3 +31,29 @@ void my_putstr(char *str)
         i++;
     }
 }
+
+static int my_disint(int nb)
+{
+    if (nb < 0){
+        my_putchar('-');
+        nb = - nb;
+    }
+    if (nb / 10 == 0){
+        my_putchar('0' + nb % 10);
+        return 0;
+    } else {
+        my_disint(nb / 10);
+        my_putchar(nb % 10 + '0');
+    }
+    return 0;
+}
+
+int my_put_nbr(int nb)
+{
+    if (nb == -2147483648){
+        my_disint(nb / 10);
+        my_putchar('8');
+    } else
+        my_disint(nb);
+    return 0;
+}
