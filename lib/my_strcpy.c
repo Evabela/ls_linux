d5f26618382e50bc6503a044ebfb2e7a8f9b2c91
Eva Belanger <eva.belanger@epitech.eu>
2025-11-04 22:31:27 +0100

    <modify> Same as my_strlowcase : condition of stop modified

--- lib/my_strcpy.c
+++ lib/my_strcpy.c
@@ -8,10 +8,10 @@ char *my_strcpy(char *dest, char const *src)
 {
     int i = 0;
 
-    while (src[i] != '\0'){
+    while (src[i] != 0){
         dest[i] = src[i];
         i++;
     }
-    dest[i] = '\0';
-    return dest;
+    dest[i] = 0;
+    return &dest[0];
 }
