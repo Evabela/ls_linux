afac789f2d371c5620e4d57a2847c8976f76d83e
Eva Belanger <eva.belanger@epitech.eu>
2025-11-04 22:25:20 +0100

    <modify> Condition of stop modified (use str[i] = 0 instead of my_strlen that wasnt working before)

--- lib/my_strlowcase.c
+++ lib/my_strlowcase.c
@@ -8,7 +8,7 @@
 
 char *my_strlowcase(char *str)
 {
-    for (int i = 0; i < my_strlen(str); i++){
+    for (int i = 0; str[i] != 0; i++){
         if (str[i] <= 'Z' && str[i] >= 'A')
             str[i] = str[i] + 32;
     }
