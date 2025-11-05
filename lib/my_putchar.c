b9a28d829e00c1d96ef375dca527c50458d32c0c
Eva Belanger <eva.belanger@epitech.eu>
2025-11-05 15:04:36 +0100

    <add> my_put_long function added in the file my_putchar

--- lib/my_putchar.c
+++ lib/my_putchar.c
@@ -57,3 +57,29 @@ int my_put_nbr(int nb)
         my_disint(nb);
     return 0;
 }
+
+static int my_dislong(long nb)
+{
+    if (nb < 0){
+        my_putchar('-');
+        nb = - nb;
+    }
+    if (nb / 10 == 0){
+        my_putchar('0' + nb % 10);
+        return 0;
+    } else {
+        my_dislong(nb / 10);
+        my_putchar(nb % 10 + '0');
+    }
+    return 0;
+}
+
+int my_put_long(long nb)
+{
+    if (nb == -2147483648){
+        my_dislong(nb / 10);
+        my_putchar('8');
+    } else
+        my_dislong(nb);
+    return 0;
+}
