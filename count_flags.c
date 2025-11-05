4b5accf64c172e92c323f0874e32c3ff58486ee4
Eva Belanger <eva.belanger@epitech.eu>
2025-11-05 11:08:33 +0100

    <add> If there's no parameter, display all the current file

--- count_flags.c
+++ count_flags.c
@@ -30,19 +30,44 @@ static int verif_flag(char letter, int *tab)
     return -1;
 }
 
-int count_flags(char **av)
+int redirect_of_flags(int nb_ac, char **av, int *tab)
+{
+    int a = 0;
+
+    if (nb_ac <= 0){
+        a = display_ls(".", tab);
+        return a;
+    }
+    for (int k = 1; k <= nb_ac; k++){
+        my_putstr(av[k]);
+        my_putstr(":\n");
+        a = display_ls(av[k], tab);
+        my_putstr("\n");
+        if (a == 84)
+            return 84;
+    }
+    return 0;
+}
+
+int count_flags(int ac, char **av)
 {
     int i = 1;
     int j = 0;
     int tab[5] = {0};
+    int a = 0;
 
-    while (av[i][0] == '-'){
+    if (ac == 1)
+        i = 0;
+    while (av[i][0] == '-' && i < ac){
         j++;
         if (av[i][j] == 0 && av[i][j - 1] != '-')
             i++;
-        if (verif_flag(av[i][j], tab) == -1){
+        if (verif_flag(av[i][j], tab) == -1)
             return 84;
-        }
     }
-    return 0;
+    if (i == 0)
+        a = redirect_of_flags(i, av, tab);
+    else
+        a = redirect_of_flags(ac - i, av, tab);
+    return a;
 }
