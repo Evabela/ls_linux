1c496e721b86e589b5692c679455ff85f6360170
Eva Belanger <eva.belanger@epitech.eu>
2025-11-05 14:58:05 +0100

    <add> Flag a finished

--- flags/flag_a.c
+++ flags/flag_a.c
@@ -6,8 +6,56 @@
 */
 
 #include "../include/my.h"
+#include <dirent.h>
+#include <stdlib.h>
+
+static char **get_files_a(char *pathname, int nb_files)
+{
+    DIR *list_files;
+    struct dirent *read_file;
+    char **list_files_ns = 0;
+    int i = 0;
+
+    list_files_ns = malloc(sizeof(char *) * nb_files);
+    list_files = opendir(pathname);
+    read_file = readdir(list_files);
+    while (read_file != 0){
+        list_files_ns[i] = malloc(sizeof(char) *
+            my_strlen(read_file->d_name));
+        my_strcpy(list_files_ns[i], read_file->d_name);
+        i++;
+        read_file = readdir(list_files);
+    }
+    closedir(list_files);
+    return list_files_ns;
+}
+
+static int count_files_a(char *pathname)
+{
+    DIR *list_files;
+    struct dirent *read_file;
+    int nb_files = 0;
+
+    list_files = opendir(pathname);
+    read_file = readdir(list_files);
+    while (read_file != 0){
+        nb_files++;
+        read_file = readdir(list_files);
+    }
+    closedir(list_files);
+    return nb_files;
+}
 
 void flag_a(char *pathname, int *tab)
 {
+    int nb_files = 0;
+    char **list_files;
 
+    nb_files = count_files_a(pathname);
+    list_files = sort_array(get_files_a(pathname, nb_files), nb_files);
+    for (int i = 0; i < nb_files; i++){
+        my_putstr(list_files[i]);
+        my_putchar('\n');
+    }
+    free(list_files);
 }
