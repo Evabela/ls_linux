f83a7f1dde11224925e79f95b3a4275cdc22723e
Eva Belanger <eva.belanger@epitech.eu>
2025-11-05 13:39:10 +0100

    <add> Error message if not file or folder found

---
+++ errors.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** errors.c
+** File description:
+** Handle all errors and return 84 if one, else 0
+*/
+
+#include "include/my.h"
+#include <fcntl.h>
+#include <dirent.h>
+
+static int error_not_exist(char *pathname, struct stat *s)
+{
+    if (S_ISDIR((*s).st_mode)){
+        if (opendir(pathname) != 0)
+            return 0;
+    }
+    if (open(pathname, O_RDONLY) >= 0){
+        return 0;
+    }
+    return 84;
+}
+
+int read_for_errors(int ac, char **av)
+{
+    struct stat s;
+
+    for (int i = 1; i < ac; i++){
+        lstat(av[i], &s);
+        if(error_not_exist(av[i], &s)){
+            my_puterror("my_ls: cannot access '");
+            my_putstr(av[i]);
+            my_putstr("' : No such file or directory\n");
+            return 84;
+        }
+    }
+    return 0;
+}
--- main.c
+++ main.c
@@ -11,6 +11,9 @@ int main(int argc, char **argv)
 {
     int a = 0;
 
+    if (argc > 1)
+        if (read_for_errors(argc, argv) == 84)
+            return 84;
     a = count_flags(argc, argv);
     return a;
 }
