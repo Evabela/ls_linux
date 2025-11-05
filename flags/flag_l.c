25914e7ee1679d91bffbe36d6e17e8bc07fd5b02
Eva Belanger <eva.belanger@epitech.eu>
2025-11-05 14:57:36 +0100

    <add> Starting of flag l, with only type and permissions for the moment

--- flags/flag_l.c
+++ flags/flag_l.c
@@ -7,7 +7,79 @@
 
 #include "../include/my.h"
 
+static void perm_oth(struct stat *s)
+{
+    if ((*s).st_mode && S_IROTH)
+        my_putchar('r');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IWOTH)
+        my_putchar('w');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IXOTH)
+        my_putchar('x');
+    else
+        my_putchar('-');
+}
+
+static void perm_grp(struct stat *s)
+{
+    if ((*s).st_mode && S_IRGRP)
+        my_putchar('r');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IWGRP)
+        my_putchar('w');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IXGRP)
+        my_putchar('x');
+    else
+        my_putchar('-');
+}
+
+static void perm_usr(struct stat *s)
+{
+    if ((*s).st_mode && S_IRUSR)
+        my_putchar('r');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IWUSR)
+        my_putchar('w');
+    else
+        my_putchar('-');
+    if ((*s).st_mode && S_IXUSR)
+        my_putchar('x');
+    else
+        my_putchar('-');
+}
+
+static void print_type(struct stat *s)
+{
+    if (S_ISREG((*s).st_mode))
+        my_putchar('-');
+    if (S_ISDIR((*s).st_mode))
+        my_putchar('d');
+    if (S_ISCHR((*s).st_mode))
+        my_putchar('c');
+    if (S_ISBLK((*s).st_mode))
+        my_putchar('b');
+    if (S_ISFIFO((*s).st_mode))
+        my_putchar('p');
+    if (S_ISLNK((*s).st_mode))
+        my_putchar('l');
+    if (S_ISSOCK((*s).st_mode))
+        my_putchar('s');
+}
+
 void flag_l(char *pathname, int *tab)
 {
-    
+    struct stat s;
+
+    lstat(pathname, &s);
+    print_type(&s);
+    perm_usr(&s);
+    perm_grp(&s);
+    perm_oth(&s);
 }
