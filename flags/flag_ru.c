ba61be107e3ef13554ecbcc9894fbd0af0b2a244
Eva Belanger <eva.belanger@epitech.eu>
2025-11-07 17:54:55 +0100

    <style> 93 column corrected in flag ru

--- flags/flag_ru.c
+++ flags/flag_ru.c
@@ -17,7 +17,8 @@ void flag_ru(char **list_files, char *pathname, int nb_files, int *tab)
         full_name = fill_full_pathname(full_name, pathname, list_files[i]);
         lstat(full_name, &type);
         if (tab[3] == 1 && S_ISDIR(type.st_mode) &&
-            my_real_strcmp("..", list_files[i]) != 0 && my_real_strcmp(".", list_files[i]) != 0){
+            my_real_strcmp("..", list_files[i]) != 0 &&
+            my_real_strcmp(".", list_files[i]) != 0){
             my_putstr(full_name);
             my_putstr(":\n");
             display_ls(full_name, tab, &type);
