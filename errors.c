af60d765099356883b76a83b2f47a3238a52cbbe
Eva Belanger <eva.belanger@epitech.eu>
2025-11-07 15:51:15 +0100

    <add> <bug> verify if we have the rights on a folder

--- errors.c
+++ errors.c
@@ -10,6 +10,33 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
+void errors_in_folder(char *pathname)
+{
+    DIR *file;
+    struct dirent *read_file;
+    struct stat s;
+
+    errno = 0;
+    file = opendir(pathname);
+    if (errno != 0){
+        perror(pathname);
+        exit(84);
+    }
+    read_file = readdir(file);
+    if (read_file == 0 && errno != 0){
+        perror(pathname);
+        exit(84);
+    }
+    while (read_file != 0 && errno == 0){
+        read_file = readdir(file);
+        if (read_file == 0 && errno != 0){
+            perror(pathname);
+            exit(84);
+        }
+    }
+}
 
 static int error_not_exist(char *pathname, struct stat *s)
 {
@@ -18,8 +45,8 @@ static int error_not_exist(char *pathname, struct stat *s)
     if (pathname[0] == '-')
         return 0;
     if (S_ISDIR((*s).st_mode)){
-        if (opendir(pathname) != 0)
-            return 0;
+        if (opendir(pathname) == 0)
+            exit(84);
     }
     if (lstat(pathname, &t) == 0){
         return 0;
--- include/my.h
+++ include/my.h
@@ -15,6 +15,7 @@ void my_puterror(char *str);
 void my_putstr(char *str);
 int my_put_nbr(int nb);
 int my_put_long(long nb);
+int my_put_long_long(long long nb);
 int my_strlen(char const *str);
 int my_intlen(int nb);
 int my_strcmp(char const *s1, char const *s2);
@@ -29,13 +30,14 @@ char **sort_array(char **list_files_ns, int nb_files);
 int read_for_errors(int ac, char **av);
 char *fill_full_pathname(char *full_pathname, char *pathname, char *file);
 void my_put_total(char **list_file, int nb_file);
+void errors_in_folder(char *pathname);
 
 int display_ls(char *pathname, int *tab, struct stat *s);
 void normal_ls(char *pathname, int *tab);
 void flag_a(char *pathname, int *tab);
 void flag_d(char *pathname, int *tab);
 void flag_l(char *pathname, int *tab);
-void flag_ru(char *pathname, int *tab);
+void flag_ru(char **list_files, char *pathname, int nb_files, int *tab);
 char **flag_t(char **list_files, char *pathname, int nb_file, int *tab);
 
 #endif /* MY_ */
