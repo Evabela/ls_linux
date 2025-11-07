6b4b9a716479ccc0101547ae1f56ebc0b3d3aea4
Eva Belanger <eva.belanger@epitech.eu>
2025-11-07 17:53:49 +0100

    <bug> Add conditions to not have an infinite loop when there's also the a flag and some minor adjustements that were missing

--- flags/flag_ru.c
+++ flags/flag_ru.c
@@ -14,13 +14,14 @@ void flag_ru(char **list_files, char *pathname, int nb_files, int *tab)
     char *full_name;
 
     for (int i = 0; i < nb_files; i++){
-        lstat(list_files[i], &type);
-        if (tab[3] == 1 && S_ISDIR(type.st_mode)){
-            full_name = fill_full_pathname(full_name, pathname, list_files[i]);
+        full_name = fill_full_pathname(full_name, pathname, list_files[i]);
+        lstat(full_name, &type);
+        if (tab[3] == 1 && S_ISDIR(type.st_mode) &&
+            my_real_strcmp("..", list_files[i]) != 0 && my_real_strcmp(".", list_files[i]) != 0){
             my_putstr(full_name);
             my_putstr(":\n");
-            free(full_name);
-            display_ls(list_files[i], tab, &type);
+            display_ls(full_name, tab, &type);
         }
+        free(full_name);
     }
 }
